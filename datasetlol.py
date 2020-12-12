d7ae64491116df149cdb69c6df591e2f16cdafd7
gustavosmont <gustavosmont@gmail.com>
2020-12-12 11:07:26 -0300

    usando notebook

--- datasetlol.py
+++ datasetlol.py
@@ -1,11 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-file = "datasetLoL\\kills.csv"
+file = "datasetLoL\\matchinfo.csv"
 dataset = pd.read_csv(file)
-print(dataset.columns)
-a = []
-b = []
-print(plt.scatter(a,b))
-plt.show()
-
+plt.show(dataset.groupby('Year').mean().plot(kind='bar'))
