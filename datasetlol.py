8c0ceaa08f0f82c3e2bef5804d47f68b879969ca
JVSenra <65605562+JVSenra@users.noreply.github.com>
2020-12-12 10:07:38 -0300

    lindão

--- datasetlol.py
+++ datasetlol.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-print("teste comit do vinição")
-print("vinicin gostoso")
+import matplotlib.pyplot as plt
 file = "datasetLoL\\kills.csv"
 dataset = pd.read_csv(file)
 print(dataset.columns)
 print(dataset.head())
+plt.scatter(dataset.x_pos,dataset.y_pos)
+
 
