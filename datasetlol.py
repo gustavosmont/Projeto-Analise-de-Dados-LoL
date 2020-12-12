9d21f9c4310c309d6c73324cc9d89b5ed425e0ba
JVSenra <65605562+JVSenra@users.noreply.github.com>
2020-12-12 20:07:11 -0300

    lindão

--- datasetlol.py
+++ datasetlol.py
@@ -5,7 +5,6 @@ import collections
 import matplotlib.pyplot as plt
 file = "datasetLoL\\matchinfo.csv"
 dataset = pd.read_csv(file)
-print(dataset.head())
 CBLol2017 = dataset[(dataset.League == 'CBLoL') & (dataset.Year == 2017) & (dataset.Season == 'Summer')]
 matchup=pd.DataFrame(columns=['Top','Jungle','Mid','ADC','Suporte','Time'])
 matchup['Top']=np.concatenate((CBLol2017.redTopChamp,CBLol2017.blueTopChamp));
@@ -15,4 +14,8 @@ matchup['ADC']=np.concatenate((CBLol2017.redADCChamp,CBLol2017.blueADCChamp));
 matchup['Suporte']=np.concatenate((CBLol2017.redSupportChamp,CBLol2017.blueSupportChamp));
 matchup['Time']=np.concatenate((CBLol2017.redTeamTag,CBLol2017.blueTeamTag));
 print("Top:",(matchup[(matchup.Time == 'PNG')].Top.describe().top),"||","Jungle:",(matchup[(matchup.Time == 'PNG')].Jungle.describe().top),"||","Mid:",(matchup[(matchup.Time == 'PNG')].Mid.describe().top),"||","ADC:",(matchup[(matchup.Time == 'PNG')].ADC.describe().top),"||","Suporte:",(matchup[(matchup.Time == 'PNG')].Suporte.describe().top))
+print((matchup[(matchup.Time == 'PNG')].describe()))
+
+
+
 
