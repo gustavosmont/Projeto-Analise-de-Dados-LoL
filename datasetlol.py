3d79bad83475f786b5e91a89d5838d7aa9988f33
JVSenra <65605562+JVSenra@users.noreply.github.com>
2020-12-12 10:12:21 -0300

    lindão

--- datasetlol.py
+++ datasetlol.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 file = "datasetLoL\\kills.csv"
 dataset = pd.read_csv(file)
 print(dataset.columns)
-print(dataset.head())
-plt.scatter(dataset.x_pos,dataset.y_pos)
-
+a = []
+b = []
+print(plt.scatter(a,b))
+plt.show()
 
