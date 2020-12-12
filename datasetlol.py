c2d8f17313aa9d353d37741752d33400ea2e5bea
JVSenra <65605562+JVSenra@users.noreply.github.com>
2020-12-12 10:00:17 -0300

    lindão

--- datasetlol.py
+++ datasetlol.py
@@ -3,6 +3,7 @@ import pandas as pd
 print("teste comit do vinição")
 print("vinicin gostoso")
 file = "datasetLoL\\kills.csv"
-dataset = pd.read_csv(file, header=0)
+dataset = pd.read_csv(file)
+print(dataset.columns)
 print(dataset.head())
 
