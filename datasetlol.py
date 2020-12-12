6ce9ba96cd6c184d2fbb4589f4c785326ffba3a4
JVSenra <65605562+JVSenra@users.noreply.github.com>
2020-12-12 13:22:26 -0300

    lindão

--- datasetlol.py
+++ datasetlol.py
@@ -1,6 +1,9 @@
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 file = "datasetLoL\\matchinfo.csv"
 dataset = pd.read_csv(file)
-plt.show(dataset.groupby('Year').mean().plot(kind='bar'))
+print(dataset.head())
+dataset.groupby('Year').gamelength.mean().plot(kind='bar')
+plt.show()
