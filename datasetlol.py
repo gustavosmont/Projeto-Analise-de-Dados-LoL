1aeec16c28fdc8d859f6dc297933a58486d7a0d7
JVSenra <65605562+JVSenra@users.noreply.github.com>
2020-12-12 09:29:31 -0300

    dataset

--- datasetlol.py
+++ datasetlol.py
@@ -1,8 +1,7 @@
-
-import pandas as pd
 import numpy as np
+import pandas as pd
 print("teste comit do vinição")
 print("vinicin gostoso")
-file="datasetLoL\\gold.csv"
-filmes = pd.read_csv(file)
-filmes.columns
+file = "datasetLoL\\gold.csv"
+filmes = pd.read_csv(file, header=0)
+print(filmes.columns)
