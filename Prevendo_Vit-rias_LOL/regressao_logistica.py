d363026feaf29536f401a4ae2b1f91193b9be003
JVSenra <65605562+JVSenra@users.noreply.github.com>
2021-03-13 09:36:53 -0300

    Criando arquivo

---
+++ Prevendo_Vit-rias_LOL/regressao_logistica.py
@@ -0,0 +1,11 @@
+# Import packages
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import seaborn as sns
+
+#Acessando o dataset padrão
+lol = "..\\datasetLoL\\LeagueofLegends.csv"
+dataset = pd.read_csv(lol)
