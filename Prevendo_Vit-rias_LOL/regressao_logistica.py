24579917c8189781c30ca96894130449c256fda6
HarryziG <viniciuslatini2001@gmail.com>
2021-03-13 12:14:44 -0300

    Regressão logistica

--- Prevendo_Vit-rias_LOL/regressao_logistica.py
+++ Prevendo_Vit-rias_LOL/regressao_logistica.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('../datasetLoL/LeagueofLegends.csv')
+df.head()
 # Import packages
 import numpy as np
 import pandas as pd
@@ -68,5 +69,35 @@ partidas = partidas[(partidas.tagAzul == 'PNG') | (partidas.tagVerm == 'PNG')]
 partidas = partidas.reset_index(drop=True)
 partidas['timeVencedor'] = np.where(partidas.resultAzul == 1, 1, 2)
 
-print(partidas)
+print(partidas.head())
+
+#Divisão de treino-teste
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(partidas[['goldAzul','killsAzul','towersAzul','inibsAzul','dragonsAzul','baronsAzul','heraldsAzul',
+                                                              'goldVerm','killsVerm','towersVerm','inibsVerm','dragonsVerm','baronsVerm','heraldsVerm']],
+                                                    partidas['timeVencedor'],test_size=0.3,random_state=42)
+#Treinando o modelo
+
+from sklearn.linear_model import LogisticRegression
+
+logmodel = LogisticRegression(max_iter=1000)
+logmodel.fit(X_train, y_train)
+
+predictions = logmodel.predict(X_test)
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+print(classification_report(y_test, predictions))
+print(confusion_matrix(y_test, predictions))
+
+ax = sns.heatmap(confusion_matrix(y_test, predictions), annot=True, fmt='d',xticklabels=['Red Team','Blue Team'], yticklabels=['Red Team','Blue Team'])
+ax.set(xlabel = 'True Label', ylabel='Predicted')
+plt.show()
+
+
+
+
 
