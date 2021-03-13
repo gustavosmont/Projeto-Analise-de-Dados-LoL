06b5bd59416a725aa9726ab000f629aec143ae94
JVSenra <65605562+JVSenra@users.noreply.github.com>
2021-03-13 19:42:00 -0300

    Finalização

--- Prevendo_Vit-rias_LOL/SVM.py
+++ Prevendo_Vit-rias_LOL/SVM.py
@@ -9,93 +9,81 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
-from ast import literal_eval
+
 """
 O caminho de acesso ao dataset escolhido é armazenado na variável lol e enviado
 através da função read_csv para a variável dataset
 """
 lol = "..\\datasetLoL\\LeagueofLegends.csv"
 dataset = pd.read_csv(lol)
+
 '''
 O seaborn é utilizado apenas para tratamentos gráficos
 '''
 sns.set_style("darkgrid", {"axes.facecolor": ".9"})
-sns.heatmap(dataset.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+sns.heatmap(dataset.isnull(), yticklabels=False, cbar=False, cmap='viridis') #Verificando a presença de dados nulos
+
 '''
 É feito uma cópia do dataset principal para um auxiliar
 '''
-dsAux = dataset.copy(deep=True)
+dsAux = dataset.copy()
 
-dsAux['goldred'] = dsAux['goldred'].apply(literal_eval)
-dsAux['goldblue'] = dsAux['goldblue'].apply(literal_eval)
-dsAux['rKills'] = dsAux['rKills'].apply(literal_eval)
-dsAux['bKills'] = dsAux['bKills'].apply(literal_eval)
-dsAux['rTowers'] = dsAux['rTowers'].apply(literal_eval)
-dsAux['bTowers'] = dsAux['bTowers'].apply(literal_eval)
-dsAux['rInhibs'] = dsAux['rInhibs'].apply(literal_eval)
-dsAux['bInhibs'] = dsAux['bInhibs'].apply(literal_eval)
-dsAux['rDragons'] = dsAux['rDragons'].apply(literal_eval)
-dsAux['bDragons'] = dsAux['bDragons'].apply(literal_eval)
-dsAux['rBarons'] = dsAux['rBarons'].apply(literal_eval)
-dsAux['bBarons'] = dsAux['bBarons'].apply(literal_eval)
-dsAux['rHeralds'] = dsAux['rHeralds'].apply(literal_eval)
-dsAux['bHeralds'] = dsAux['bHeralds'].apply(literal_eval)
 '''
 O nosso dataframe principal é criado com o nome de 'partidas'
 '''
-partidas = pd.DataFrame()
+partidas = pd.DataFrame(columns=['tagAzul','ResultadoAzul','OuroAzul','killsAzul','TorresAzul','InibidoresAzul','DragoesAzul','BaroesAzul','ArautosAzul','tagVerm','ResultadoVerm','OuroVerm','killsVerm','TorresVerm','InibidoresVerm','DragoesVerm','BaroesVerm','ArautosVerm','TimeVencedor'])
+
 '''
 As colunas do dataframe são criadas e preenchidas com os dados advindos do dataset auxiliar
-Para as informações de Gold (moedas), é utilizada a função apply(max) para que seja obtido
+Para as informações de Gold (Ouro), é utilizada a função apply(max) para que seja obtido
 o valor máximo de dinheiro que aquele determinado time alcançou naquela partida
 Para a quantidade de kills, torres, inibidores, dragões, barões e arautos, é utilizada a função apply(len)
 para obter a quantidade de ocorrências de cada coluna.
 Ou seja, se há 6 registros de torres destruídas naquela linha, então 6 torres foram destruídas no total
 '''
-partidas['tagAzul'] = dsAux['blueTeamTag']
-partidas['resultAzul'] = dsAux['bResult']
-partidas['goldAzul'] = dsAux['goldblue'].apply(max)
-partidas['killsAzul'] = dsAux['bKills'].apply(len)
-partidas['towersAzul'] = dsAux['bTowers'].apply(len)
-partidas['inibsAzul'] = dsAux['bInhibs'].apply(len)
-partidas['dragonsAzul'] = dsAux['bDragons'].apply(len)
-partidas['baronsAzul'] = dsAux['bBarons'].apply(len)
-partidas['heraldsAzul'] = dsAux['bHeralds'].apply(len)
+partidas.tagAzul = dsAux.blueTeamTag
+partidas.ResultadoAzul = dsAux.bResult
+partidas.OuroAzul = (dsAux.goldblue.apply(eval)).apply(max)
+partidas.killsAzul = (dsAux.bKills.apply(eval)).apply(len)
+partidas.TorresAzul = (dsAux.bTowers.apply(eval)).apply(len)
+partidas.InibidoresAzul = (dsAux.bInhibs.apply(eval)).apply(len)
+partidas.DragoesAzul =(dsAux.bDragons.apply(eval)).apply(len)
+partidas.BaroesAzul = (dsAux.bBarons.apply(eval)).apply(len)
+partidas.ArautosAzul = (dsAux.bHeralds.apply(eval)).apply(len)
 '''
 O mesmo ocorre para o lado vermelho
 '''
-partidas['tagVerm'] = dsAux['redTeamTag']
-partidas['resultVerm'] = dsAux['rResult']
-partidas['goldVerm'] = dsAux['goldred'].apply(max)
-partidas['killsVerm'] = dsAux['rKills'].apply(len)
-partidas['towersVerm'] = dsAux['rTowers'].apply(len)
-partidas['inibsVerm'] = dsAux['rInhibs'].apply(len)
-partidas['dragonsVerm'] = dsAux['rDragons'].apply(len)
-partidas['baronsVerm'] = dsAux['rBarons'].apply(len)
-partidas['heraldsVerm'] = dsAux['rHeralds'].apply(len)
+partidas.tagVerm = dsAux.redTeamTag
+partidas.ResultadoVerm = dsAux.rResult
+partidas.OuroVerm = (dsAux.goldred.apply(eval)).apply(max)
+partidas.killsVerm = (dsAux.rKills.apply(eval)).apply(len)
+partidas.TorresVerm = (dsAux.rTowers.apply(eval)).apply(len)
+partidas.InibidoresVerm = (dsAux.rInhibs.apply(eval)).apply(len)
+partidas.DragoesVerm = (dsAux.rDragons.apply(eval)).apply(len)
+partidas.BaroesVerm = (dsAux.rBarons.apply(eval)).apply(len)
+partidas.ArautosVerm = (dsAux.rHeralds.apply(eval)).apply(len)
+
 '''
 Aqui filtramos o dataframe, restringindo apenas aos jogos da equipe brasileira PAIN Gaming
 '''
 partidas = partidas[(partidas.tagAzul == 'PNG') | (partidas.tagVerm == 'PNG')]
-partidas = partidas.reset_index(drop=True)
+
 '''
 Para saber qual time daquela partida venceu (do lado azul ou do lado vermelho), utilizou-se a função
 np.where e, caso a coluna 'resultAzul' for igual a 1, significa que o lado azul venceu o jogo, então
 a coluna timeVencedor recebe 1. Caso contrário, o time vermelho que venceu, então a coluna recebe 2.
 '''
-partidas['timeVencedor'] = np.where(partidas.resultAzul == 1, 1, 2)
+partidas.TimeVencedor = np.where(partidas.ResultadoAzul == 1, 1, 2)
 
 '''
 Divisão de Treino e Teste com as colunas que influenciam no resultado de um jogo.
-(Quantidade de Gold; kills; torres e inibidores destruídos; dragões, barões e arautos abatidos)
+(Quantidade de Gold; kills; torres e inibidores destruídos; dragões, barões e arautos abatidos).
+O treino foi realizado em uma proporção de 7:3.
 '''
-x = partidas[['goldAzul', 'killsAzul', 'towersAzul', 'inibsAzul', 'dragonsAzul', 'baronsAzul', 'heraldsAzul',
-            'goldVerm', 'killsVerm', 'towersVerm', 'inibsVerm', 'dragonsVerm', 'baronsVerm', 'heraldsVerm']]
-y = partidas['timeVencedor']
-'''
-O treino foi realizado em uma proporção de 7:3
-'''
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state=42)
+
+X_train, X_test, y_train, y_test = train_test_split(partidas[['OuroAzul','killsAzul','TorresAzul','InibidoresAzul','DragoesAzul','BaroesAzul','ArautosAzul',
+                                                              'OuroVerm','killsVerm','TorresVerm','InibidoresVerm','DragoesVerm','BaroesVerm','ArautosVerm']],
+                                                    partidas['TimeVencedor'],test_size=0.30,random_state=101)
 '''
 Os códigos abaixo aplicam o GridSearch para analisar quais os parâmetros mais eficientes do SVM
 quando se analisa os dados do dataframe de partidas. Os mesmos foram comentados pois levam certo tempo
@@ -107,21 +95,24 @@ de treino do SVC
 # grid = GridSearchCV(SVC(), params_grid, refit=True, verbose=3)
 # grid.fit(x_train, y_train)
 # print(grid.best_estimator_)
+
 '''
 Os parâmetros de melhor estimativa foram C=0.1, degree=1, gamma=1, kernel='linear'
 Sendo assim, criou-se o modelo SVC e o mesmo foi treinado em seguida
 '''
 modelo = SVC(C=0.1, gamma=1, kernel='linear', degree=1, random_state=10)
-modelo.fit(x_train, y_train)
+modelo.fit(X_train, y_train)
+
 '''
 Deu-se início as predições dos resultados alcançados no treino, após finalizadas,
 ocorreu o print do 'classification report'
 '''
-prediction = modelo.predict(x_test)
+prediction = modelo.predict(X_test)
 print(classification_report(y_test, prediction, zero_division=0))
+
 '''
 Por último, a matriz de confusão foi plotada em forma gráfica para melhor visualização nas linhas abaixo
 '''
-ax = sns.heatmap(confusion_matrix(y_test, prediction), annot=True, fmt='d',xticklabels=['Red Team','Blue Team'], yticklabels=['Red Team','Blue Team'])
+ax = sns.heatmap(confusion_matrix(y_test, prediction), annot=True, fmt='d',xticklabels=['Time Vermelho','Time Azul'], yticklabels=['Time Vermelho','Time Azul'])
 ax.set(xlabel = 'True Label', ylabel='Predicted')
 plt.show()
--- Prevendo_Vit-rias_LOL/regressao_logistica.py
+++ Prevendo_Vit-rias_LOL/regressao_logistica.py
@@ -1,96 +1,108 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv('../datasetLoL/LeagueofLegends.csv')
-df.head()
-# Import packages
+"""
+O código desse arquivo realiza o preparo do dataset para obter as informações necessárias.
+Primeiramente, foram importadas todas as bibliotecas, classes e funções requeridas.
+"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
-
-#Acessando o dataset padrão
-lol = "..\\datasetLoL\\LeagueofLegends.csv"
-dataset = pd.read_csv(lol)
-#print(dataset.info()) #Analisando as colunas do dataset e verificando os tipos
-
-'''sns.set_style("darkgrid", {"axes.facecolor": ".9"})
-sns.heatmap(dataset.isnull(), yticklabels=False, cbar=False, cmap='viridis') #Verificando a presença de dados nulos
-plt.show()'''
-
-dsAux = dataset.copy()
-
-#Colunas a que influenciam na vitórias de uma partida: gold, kills, towers, inhibs, dragons, barons, heralds
-print(dsAux.info())
-dsAux['goldred'] = dsAux['goldred'].apply(eval)
-dsAux['goldblue'] = dsAux['goldblue'].apply(eval)
-dsAux['rKills'] = dsAux['rKills'].apply(eval)
-dsAux['bKills'] = dsAux['bKills'].apply(eval)
-dsAux['rTowers'] = dsAux['rTowers'].apply(eval)
-dsAux['bTowers'] = dsAux['bTowers'].apply(eval)
-dsAux['rInhibs'] = dsAux['rInhibs'].apply(eval)
-dsAux['bInhibs'] = dsAux['bInhibs'].apply(eval)
-dsAux['rDragons'] = dsAux['rDragons'].apply(eval)
-dsAux['bDragons'] = dsAux['bDragons'].apply(eval)
-dsAux['rBarons'] = dsAux['rBarons'].apply(eval)
-dsAux['bBarons'] = dsAux['bBarons'].apply(eval)
-dsAux['rHeralds'] = dsAux['rHeralds'].apply(eval)
-dsAux['bHeralds'] = dsAux['bHeralds'].apply(eval)
-print(dsAux.info())
-
-partidas = pd.DataFrame()
-partidas['tagAzul'] = dsAux['blueTeamTag']
-partidas['resultAzul'] = dsAux['bResult']
-partidas['goldAzul'] = dsAux['goldblue'].apply(max)
-partidas['killsAzul'] = dsAux['bKills'].apply(len)
-partidas['towersAzul'] = dsAux['bTowers'].apply(len)
-partidas['inibsAzul'] = dsAux['bInhibs'].apply(len)
-partidas['dragonsAzul'] = dsAux['bDragons'].apply(len)
-partidas['baronsAzul'] = dsAux['bBarons'].apply(len)
-partidas['heraldsAzul'] = dsAux['bHeralds'].apply(len)
-partidas['tagVerm'] = dsAux['redTeamTag']
-partidas['resultVerm'] = dsAux['rResult']
-partidas['goldVerm'] = dsAux['goldred'].apply(max)
-partidas['killsVerm'] = dsAux['rKills'].apply(len)
-partidas['towersVerm'] = dsAux['rTowers'].apply(len)
-partidas['inibsVerm'] = dsAux['rInhibs'].apply(len)
-partidas['dragonsVerm'] = dsAux['rDragons'].apply(len)
-partidas['baronsVerm'] = dsAux['rBarons'].apply(len)
-partidas['heraldsVerm'] = dsAux['rHeralds'].apply(len)
-
-partidas = partidas[(partidas.tagAzul == 'PNG') | (partidas.tagVerm == 'PNG')]
-partidas = partidas.reset_index()
-partidas['timeVencedor'] = np.where(partidas.resultAzul == 1, 1, 2)
-
-print(partidas.head())
-
-#Divisão de treino-teste
-
 from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(partidas[['goldAzul','killsAzul','towersAzul','inibsAzul','dragonsAzul','baronsAzul','heraldsAzul',
-                                                              'goldVerm','killsVerm','towersVerm','inibsVerm','dragonsVerm','baronsVerm','heraldsVerm']],
-                                                    partidas['timeVencedor'],test_size=0.30,random_state=42)
-#Treinando o modelo
-
 from sklearn.linear_model import LogisticRegression
-
-logmodel = LogisticRegression(max_iter=1000)
-logmodel.fit(X_train, y_train)
-
-predictions = logmodel.predict(X_test)
-
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
-print(classification_report(y_test, predictions))
-print(confusion_matrix(y_test, predictions))
+"""
+O caminho de acesso ao dataset escolhido é armazenado na variável lol e enviado
+através da função read_csv para a variável dataset
+"""
+lol = "..\\datasetLoL\\LeagueofLegends.csv"
+dataset = pd.read_csv(lol)
 
-ax = sns.heatmap(confusion_matrix(y_test, predictions), annot=True, fmt='d',xticklabels=['Red Team','Blue Team'], yticklabels=['Red Team','Blue Team'])
+'''
+O seaborn é utilizado apenas para tratamentos gráficos
+'''
+sns.set_style("darkgrid", {"axes.facecolor": ".9"})
+sns.heatmap(dataset.isnull(), yticklabels=False, cbar=False, cmap='viridis') #Verificando a presença de dados nulos
+
+'''
+É feito uma cópia do dataset principal para um auxiliar
+'''
+dsAux = dataset.copy()
+
+'''
+O nosso dataframe principal é criado com o nome de 'partidas'
+'''
+partidas = pd.DataFrame(columns=['tagAzul','ResultadoAzul','OuroAzul','killsAzul','TorresAzul','InibidoresAzul','DragoesAzul','BaroesAzul','ArautosAzul','tagVerm','ResultadoVerm','OuroVerm','killsVerm','TorresVerm','InibidoresVerm','DragoesVerm','BaroesVerm','ArautosVerm','TimeVencedor'])
+
+'''
+As colunas do dataframe são criadas e preenchidas com os dados advindos do dataset auxiliar
+Para as informações de Gold (Ouro), é utilizada a função apply(max) para que seja obtido
+o valor máximo de dinheiro que aquele determinado time alcançou naquela partida
+Para a quantidade de kills, torres, inibidores, dragões, barões e arautos, é utilizada a função apply(len)
+para obter a quantidade de ocorrências de cada coluna.
+Ou seja, se há 6 registros de torres destruídas naquela linha, então 6 torres foram destruídas no total
+'''
+partidas.tagAzul = dsAux.blueTeamTag
+partidas.ResultadoAzul = dsAux.bResult
+partidas.OuroAzul = (dsAux.goldblue.apply(eval)).apply(max)
+partidas.killsAzul = (dsAux.bKills.apply(eval)).apply(len)
+partidas.TorresAzul = (dsAux.bTowers.apply(eval)).apply(len)
+partidas.InibidoresAzul = (dsAux.bInhibs.apply(eval)).apply(len)
+partidas.DragoesAzul =(dsAux.bDragons.apply(eval)).apply(len)
+partidas.BaroesAzul = (dsAux.bBarons.apply(eval)).apply(len)
+partidas.ArautosAzul = (dsAux.bHeralds.apply(eval)).apply(len)
+'''
+O mesmo ocorre para o lado vermelho
+'''
+partidas.tagVerm = dsAux.redTeamTag
+partidas.ResultadoVerm = dsAux.rResult
+partidas.OuroVerm = (dsAux.goldred.apply(eval)).apply(max)
+partidas.killsVerm = (dsAux.rKills.apply(eval)).apply(len)
+partidas.TorresVerm = (dsAux.rTowers.apply(eval)).apply(len)
+partidas.InibidoresVerm = (dsAux.rInhibs.apply(eval)).apply(len)
+partidas.DragoesVerm = (dsAux.rDragons.apply(eval)).apply(len)
+partidas.BaroesVerm = (dsAux.rBarons.apply(eval)).apply(len)
+partidas.ArautosVerm = (dsAux.rHeralds.apply(eval)).apply(len)
+
+'''
+Aqui filtramos o dataframe, restringindo apenas aos jogos da equipe brasileira PAIN Gaming
+'''
+partidas = partidas[(partidas.tagAzul == 'PNG') | (partidas.tagVerm == 'PNG')]
+
+'''
+Para saber qual time daquela partida venceu (do lado azul ou do lado vermelho), utilizou-se a função
+np.where e, caso a coluna 'resultAzul' for igual a 1, significa que o lado azul venceu o jogo, então
+a coluna timeVencedor recebe 1. Caso contrário, o time vermelho que venceu, então a coluna recebe 2.
+'''
+partidas.TimeVencedor = np.where(partidas.ResultadoAzul == 1, 1, 2)
+
+
+'''
+Divisão de Treino e Teste com as colunas que influenciam no resultado de um jogo.
+(Quantidade de Gold; kills; torres e inibidores destruídos; dragões, barões e arautos abatidos).
+O treino foi realizado em uma proporção de 7:3.
+'''
+
+X_train, X_test, y_train, y_test = train_test_split(partidas[['OuroAzul','killsAzul','TorresAzul','InibidoresAzul','DragoesAzul','BaroesAzul','ArautosAzul',
+                                                              'OuroVerm','killsVerm','TorresVerm','InibidoresVerm','DragoesVerm','BaroesVerm','ArautosVerm']],
+                                                    partidas['TimeVencedor'],test_size=0.30,random_state=101)
+'''
+Criou-se o modelo Regressão Logistica e o mesmo foi treinado em seguida
+'''
+logmodel = LogisticRegression()
+logmodel.fit(X_train, y_train)
+
+'''
+Deu-se início as predições dos resultados alcançados no treino, após finalizadas,
+ocorreu o print do 'classification report'
+'''
+predictions = logmodel.predict(X_test)
+print(classification_report(y_test, predictions))
+
+'''
+Por último, a matriz de confusão foi plotada em forma gráfica para melhor visualização nas linhas abaixo
+'''
+ax = sns.heatmap(confusion_matrix(y_test, predictions), annot=True, fmt='d',xticklabels=['Time Vermelho','Time Azul'], yticklabels=['Time Vermelho','Time Azul'])
 ax.set(xlabel = 'True Label', ylabel='Predicted')
 plt.show()
 
